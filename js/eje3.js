474804b3d7526e7833e277eebd0bcb5eb0e92725
Tadeo Munoz <tadeomunoz09@hotmail.com>
2023-03-11 17:59:21 -0300

    realice el ejercicio 3

---
+++ js/eje3.js
@@ -0,0 +1,12 @@
+/*3- Escribir un script que simule el lanzamiento de dos dados.
+ Hacer uso de la función Math.random para obtener números aleatorios entre 1 y 6 para cada uno de los lanzamientos de los dados.
+  Sumar el resultado de lanzar dos dados y anotar en un array el número de apariciones de dicha suma, repitiendo 50 veces esta operación.
+ */
+
+let resultados = [];
+
+for(let i = 0; i < 50; i ++){
+    resultados[i] = (Math.floor(Math.random()*6) + 1) + (Math.floor(Math.random()*6) + 1);
+    console.log(resultados[i]);
+}
+
