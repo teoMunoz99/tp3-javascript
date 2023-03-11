d90a975be0be3c1bb227c40b7ae6d9fe56afe651
Tadeo Munoz <tadeomunoz09@hotmail.com>
2023-03-11 16:32:15 -0300

    realice el ejercicio 1

---
+++ js/eje1.js
@@ -0,0 +1,9 @@
+/*1- Crear un array llamado meses y que almacene el nombre de los doce meses del año. 
+    Mostrar por pantalla en forma de lista  los doce nombres del arreglo.*/
+
+
+let meses = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
+
+for(let i = 0; i < meses.length; i++){
+    console.log(meses[i]);
+}
